3155f63622820c971d38f6c86f31fe71a1e6f905
rucks15 <rucks15@gmail.com>
2018-10-20 12:39:50 -0700

    client render-dom and axios added

--- client/src/App.js
+++ client/src/App.js
@@ -2,17 +2,26 @@ import React, { Component } from 'react';
 import Navbar from './components/layouts/navbar';
 import Footer from './components/layouts/footer';
 import Landing from './components/layouts/landing';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Login from './components/auth/login';
+import Register from './components/auth/register';
 
 import './App.css';
 
 class App extends Component {
   render() {
     return (
+      <Router>
       <div className="App">
         <Navbar />
-        <Landing />
+        <Route exact path = "/" component = { Landing } />
+        <div className="container">
+          <Route exact path = "/register" component={Register}/>
+          <Route exact path = "/login" component={Login}/>
+        </div>
         <Footer />
       </div>
+      </Router>
     );
   }
 }
---
+++ client/src/components/auth/login.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+ class Login extends Component {
+  render() {
+    return (
+      <div>
+        <hi> Login </hi>
+      </div>
+    )
+  }
+}
+export default Login;
